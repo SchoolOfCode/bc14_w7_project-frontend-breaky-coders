8e1adcc597b7a70a239b863db03b606bf97b3479
gipo-ski <koladiji@gmail.com>
2023-04-26 12:04:31 +0100

    added components folders for app pages: AddFlashcard, HomePage, MyFlashcards, RandomFlashCard  and was able to link pages after updating main App.js to frontend-project

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,41 +1,70 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-import bearTitleImage from '../../assets/bearTitleImage.png'
-import RandomFlashCard from '../RandomFlashCard';
-import flashCardsListData from './stateData';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import HomePage from "../HomePage";
+import MyFlashcards from "../MyFlashcards";
 
 function App() {
-  
-  // this is the state for the flashcards the initial value is the data from the stateData.js file
-  const [flashCardsList, setFlashCards] = useState(flashCardsListData); 
-  return (
-    <div className="App">
-      <div className="overlay">
+  const [page, setPage] = useState("homepage");
 
-        <header className="header">
-        <div className="header__image">
-          <img src={bearTitleImage} alt="bearTitleImage" />
-        </div>
-        </header>
-        <main>
-          <RandomFlashCard flashcards={flashCardsList} flashCardsListData={flashCardsListData}/>
+  function renderPage() {
+    if (page === "homepage") {
+      return <HomePage setPage={setPage} />;
+    } else if (page === "myFlashcards") {
+      return <MyFlashcards setPage={setPage} />;
+    }
+  }
 
-          <nav className="nav__buttons">
-            <div className="nav__buttons-button">
-              <button>Add A Flashcard</button>
-            </div>
-            <div className="nav__buttons-button">
-              <button>My Flashcards</button>
-            </div>
-            <div className="nav__buttons-button">
-              <button>Test Me</button>
-            </div>
-          </nav>
-        </main>
-      </div>
-    </div> 
-  );
+  return <>{renderPage()};</>;
 }
 
 export default App;
+
+
+
+
+
+
+
+//______________________________________________
+
+/*
+all the codes below were written before while testing out how to link the pages together in React
+
+// import MyFlashcards from '../MyFlashcards';
+// import bearTitleImage from '../../assets/bearTitleImage.png'
+// import RandomFlashCard from '../RandomFlashCard';
+// import flashCardsListData from '../HomePage/stateData';
+// this is the state for the flashcards the initial value is the data from the stateData.js file
+// const [flashCardsList, setFlashCards] = useState(flashCardsListData); 
+
+
+// <div className="App">
+    //   <div className="overlay">
+
+    //     <header className="header">
+    //     <div className="header__image">
+    //       <img src={bearTitleImage} alt="bearTitleImage" />
+    //     </div>
+    //     </header>
+    //     <main>
+    //       <RandomFlashCard flashcards={flashCardsList} flashCardsListData={flashCardsListData}/>
+
+    //       <nav className="nav__buttons">
+    //         <div className="nav__buttons-button">
+    //           <button>Add A Flashcard</button>
+    //         </div>
+    //         <div className="nav__buttons-button">
+    //           <button>My Flashcards</button>
+    //         </div>
+    //         <div className="nav__buttons-button">
+    //           <button>Test Me</button>
+    //         </div>
+    //       </nav>
+    //     </main>
+    //   </div>
+    // </div> 
+
+
+*/
+//_____________________________________________________
---
+++ my-app/src/components/MyFlashcards/index.js
@@ -0,0 +1,9 @@
+import App from "../App/App";
+
+
+function MyFlashcards() {
+    
+return (<h1>Hello There</h1> )
+}
+
+export default MyFlashcards
