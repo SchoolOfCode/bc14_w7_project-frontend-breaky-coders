d5c5b761019ad433ee87c61e8bb94f51efe377c3
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-04-26 16:22:57 +0100

    My Flashcard Page Done

--- my-app/src/components/MyFlashcards/index.js
+++ my-app/src/components/MyFlashcards/index.js
@@ -1,14 +1,27 @@
 import Flashcard from "../Flashcard/"
+import "./MyFlashcards.css"
 
-function MyFlashcards({flashcardsList}) {
+function MyFlashcards({flashcardsList, setPage}) {
     
   return (
-    <div className="overlay">
+    <div className="flashcardsOverlay">
+
+        <div className="overlay__header">
+
+            <div className="overlay__header-heading">
+                <h1>My Flashcards</h1>
+            </div>
+            <div className="overlay__header-button">
+
+                <button onClick={() => {setPage("homepage")}}>Go Back</button>
+
+            </div>
+        </div>
         <div className="overlay__content">
 
-      {flashcardsList.map((flashcard, index) => {
-        return <Flashcard key={index} flashcardList={flashcard}/>
-      })}
+            {flashcardsList.map((flashcard, index) => {
+                return <Flashcard key={index} flashcardList={flashcard}/>
+            })}
 
         </div>
     </div>
